cead3a0ae3640b9dff4d26c191ad1cbab55f096b
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2024-08-30 12:21:57 +0200

    Adds verification endpoint

--- httpserver/handler.go
+++ httpserver/handler.go
@@ -2,12 +2,17 @@ package httpserver
 
 import (
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/go-chi/chi/v5"
+	tdx_abi "github.com/google/go-tdx-guest/abi"
 	"github.com/google/go-tdx-guest/client"
+	tdx_pb "github.com/google/go-tdx-guest/proto/tdx"
+	"github.com/google/go-tdx-guest/verify"
 	"github.com/ruteri/dummy-tdx-dcap/metrics"
 )
 
@@ -20,10 +25,10 @@ func DummyAttest(appdata [64]byte) ([]byte, error) {
 	return appdata[:], nil
 }
 
-func (s *Server) handleAPI(w http.ResponseWriter, r *http.Request) {
+func (s *Server) handleAttest(w http.ResponseWriter, r *http.Request) {
 	m := s.metricsSrv.Float64Histogram(
 		"request_duration_api",
-		"API request handling duration",
+		"attest handling duration",
 		metrics.UomMicroseconds,
 		metrics.BucketsRequestDuration...,
 	)
@@ -53,6 +58,61 @@ func (s *Server) handleAPI(w http.ResponseWriter, r *http.Request) {
 	_, err = w.Write(quote)
 	if err != nil {
 		log.Error("could not send back the quote", "err", err)
+	}
+}
+
+func (s *Server) handleVerify(w http.ResponseWriter, r *http.Request) {
+	m := s.metricsSrv.Float64Histogram(
+		"request_duration_api",
+		"verify handling duration",
+		metrics.UomMicroseconds,
+		metrics.BucketsRequestDuration...,
+	)
+	defer func(start time.Time) {
+		m.Record(r.Context(), float64(time.Since(start).Microseconds()))
+	}(time.Now())
+
+	var rawQuoteData [1280]byte // 1020 is min size, what's the max size?
+	n, err := r.Body.Read(rawQuoteData[:])
+	if n == 1280 {
+		http.Error(w, "quote too large", http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	protoQuote, err := tdx_abi.QuoteToProto(rawQuoteData[:])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	v4Quote, err := func() (*tdx_pb.QuoteV4, error) {
+		switch q := protoQuote.(type) {
+		case *tdx_pb.QuoteV4:
+			return q, nil
+		default:
+			return nil, fmt.Errorf("unsupported quote type: %T", q)
+		}
+	}()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	options := verify.DefaultOptions()
+	// TODO: fetch colateral before verifying to distinguish the error better
+	err = verify.TdxQuote(protoQuote, options)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusTeapot)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(v4Quote)
+	if err != nil {
+		log.Error("could not respond to /verify", "err", err)
 	}
 }
 
